bb11d659998248c855c7026b021731c13c65f74b
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-08-22 21:05:55 +0300

    Make the player move horizontally using the UI buttons

---
+++ Assets/Scripts/TargetFollower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HillClimbRacingPrototype
+{
+    public class TargetFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 offset;
+
+        private void LateUpdate() => FollowTarget();
+
+        private void FollowTarget() => transform.position = target.position + offset;
+    }
+}
