dcb725ba0f8521428465ce19244fd3cf32f463a0
Yuriy Danyliuk <danyliuk.yuriy@gmail.com>
2023-08-22 20:45:26 +0300

    Handle UI buttons events

---
+++ Assets/Scripts/UI/ButtonEventsHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace HillClimbRacingPrototype.UI
+{
+    public class ButtonEventsHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public bool IsPressed { get; private set; }
+
+        public void OnPointerDown(PointerEventData eventData) => IsPressed = true;
+
+        public void OnPointerUp(PointerEventData eventData) => IsPressed = false;
+    }
+}
